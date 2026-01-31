88d0d08065acb9e8631ff02a79ddfe204d06c433
aigentone <aigentone@icloud.com>
2026-01-31 00:02:27 -0500

    add

--- legacy/governance-simulation/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data models for the DAHAO sidecar."""
-
-from models.proposal import Proposal, ProposalStatus
-from models.vote import VoteChoice, VoteDecision
-
-__all__ = ["Proposal", "ProposalStatus", "VoteChoice", "VoteDecision"]
